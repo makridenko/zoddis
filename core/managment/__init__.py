03388b53132e80ca1ee98b184f26da148edeb4ec
Alex M <makridenko.a@yandex.ru>
2019-10-16 01:38:08 +0300

    template

---
+++ core/managment/__init__.py
@@ -0,0 +1,25 @@
+# Global modules
+import sys
+
+# All commands
+from .helpers import commands
+from .commands.help import help
+
+class ManageUtil:
+    def __init__(self, argv=None):
+        self.argv = argv or sys.argv[:]
+
+    def default(self):
+        help()
+
+    def execute(self):
+        try:
+            subcommand = self.argv[1]
+            commands.get(subcommand, self.default)()
+        except IndexError:
+            help()
+
+
+def execute_from_command_line(argv=None):
+    util = ManageUtil(argv)
+    util.execute()
---
+++ core/managment/helpers.py
@@ -0,0 +1,8 @@
+# Commands
+from .commands.help import help
+
+
+commands = {
+    '-h': help,
+    '--help': help,
+}
